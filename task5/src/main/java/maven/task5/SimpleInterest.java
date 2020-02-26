c7fe18f0036f75bc458b0f46b896c5ac8cfe3420
Mudigonda Sri Harsha <sonusri.harsha143@gmail.com>
2020-02-26 21:32:12 +0530

    first commit

---
+++ task5/src/main/java/maven/task5/SimpleInterest.java
@@ -0,0 +1,9 @@
+package maven.task5;
+
+public class SimpleInterest {
+	float simpleInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return ((principle*time*rateOfInterest)/100);
+	}
+
+}
